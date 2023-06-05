d4c610778f55606224e5249b86114b0b51ef8dbd
Malcolm O <malcolmohare@hotmail.com>
2023-06-04 21:38:12 -0400

    add collection model and controller and read views

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'collections/index'
+  get 'collections/:id', to: 'collections#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
