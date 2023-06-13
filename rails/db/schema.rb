f2c3a5b846a3524d3056b3ac55b28870ea91c897
Malcolm O <malcolmohare@hotmail.com>
2023-06-12 20:24:12 -0400

    add devise and generate default user setup

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_011403) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_002002) do
   create_table "collection_items", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_011403) do
     t.datetime "updated_at", null: false
     t.integer "collection_type_id"
     t.index ["collection_type_id"], name: "index_collections_on_collection_type_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "collection_items", "collections"
