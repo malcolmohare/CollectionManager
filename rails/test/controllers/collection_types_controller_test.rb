fcc084913e79940ea29323c7db45c39fad967dea
Malcolm O'Hare <malcolmohare@hotmail.com>
2024-04-12 23:38:16 -0400

    collection type contoller tests (#22)

--- rails/test/controllers/collection_types_controller_test.rb
+++ rails/test/controllers/collection_types_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class CollectionTypesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @collection_type = collection_types(:one)
+  end
+
+  test "should get index" do
+    get collection_types_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get collection_type_url(@collection_type)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_collection_type_url
+    assert_response :success
+  end
+
+  test "should create collection_type" do
+    assert_difference('CollectionType.count') do
+      post collection_types_url, params: { collection_type: { name: "Test Collection Type" } }
+    end
+
+    assert_redirected_to collection_type_url(CollectionType.last)
+  end
 end
