dad85976280d4902ac0bfeaec6a79794ca3c1050
Malcolm O'Hare <malcolmohare@hotmail.com>
2024-05-20 19:19:07 -0400

    Mo/more tests (#32)
    * remove test files from coverage
    
    * more test coverage

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   get '/collections/:id/uncollect', to: 'collections#uncollect', as: 'uncollect_collection'
   get '/collections/:id/bulk_create_items', to: 'collections#bulk_create_items', as: 'bulk_create_items'
   post '/collections/:id/bulk_create_items', to: 'collections#process_bulk_create_items', as: 'process_bulk_create_items'
-  get '/items/:id/collect', to: 'items#collect'
-  get '/items/:id/uncollect', to: 'items#uncollect'
+  get '/items/:id/collect', to: 'items#collect', as: 'collect_item'
+  get '/items/:id/uncollect', to: 'items#uncollect', as: 'uncollect_item'
   get '/admin', to: 'admin#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- rails/test/controllers/items_controller_test.rb
+++ rails/test/controllers/items_controller_test.rb
@@ -27,4 +27,78 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     @item.reload
     assert_equal "New Name", @item.name
   end
+
+  test "should show item" do
+    get item_url(@item)
+    assert_response :success
+  end
+
+  test "should get new" do
+    sign_in @user
+    get new_item_url
+    assert_response :success
+  end
+
+  test "should create item" do
+    sign_in @user
+    assert_difference('Item.count') do
+      post items_url, params: { item: { name: "New Item", collection_id: @item.collection_id } }
+    end
+
+    assert_redirected_to collection_url(@item.collection)
+  end
+
+  test "should not create item if not logged in" do
+    assert_no_difference('Item.count') do
+      post items_url, params: { item: { name: "New Item", collection_id: @item.collection_id } }
+    end
+
+    assert_redirected_to new_user_session_url
+  end
+
+  test "should get edit" do
+    sign_in @user
+    get edit_item_url(@item)
+    assert_response :success
+  end
+
+  test "should not get edit if not authorized" do
+    sign_in users(:two)
+    get edit_item_url(@item)
+    assert_redirected_to items_url
+    assert_not_nil flash[:notice]
+  end
+
+  test "should update item" do
+    sign_in @user
+    patch item_url(@item), params: { item: { name: "Updated Item" } }
+    assert_redirected_to item_url(@item)
+    @item.reload
+    assert_equal "Updated Item", @item.name
+  end
+
+  test "should not update item if not authorized" do
+    sign_in users(:two)
+    patch item_url(@item), params: { item: { name: "Updated Item" } }
+    assert_redirected_to items_url
+    assert_not_nil flash[:notice]
+  end
+
+  test "should collect item" do
+    item = items(:two)
+    sign_in @user
+    assert_difference('UserItem.count') do
+      get collect_item_url(item)
+    end
+    assert_response :success
+  end
+
+  test "should uncollect item" do
+    sign_in @user
+    UserItem.create(user: @user, item: @item)
+    assert_difference('UserItem.count', -1) do
+      get uncollect_item_url(@item)
+    end
+    assert_response :success
+  end
 end
